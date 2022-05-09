b529d793215d909cd2bea25d844d4948f5717f5f
bobmustaev <91593059+bobmustaev@users.noreply.github.com>
2022-05-09 10:52:36 +0200

    Add files via upload

--- src/main/java/com/opencelium/csvtoapi/ApiController.java
+++ src/main/java/com/opencelium/csvtoapi/ApiController.java
@@ -8,6 +8,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -21,13 +24,19 @@ public class ApiController {
     @GetMapping("/")
     public ResponseEntity<?> get(@RequestParam Map<String, String> queryParams){
         Arrays.asList(CsvDocument.class.getDeclaredFields()).forEach(f -> System.out.println(f.getName()));
-        String source = queryParams.get("source").replaceAll("%26", "&");
+        String source = queryParams.get("source");
+        try {
+            source = decodeValue(source);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
         Format format = getFormat(queryParams.get("format"));
         String sort = queryParams.get("sort");
         String sort_dir = queryParams.get("sort_dir");
         String filter = getFilter(queryParams);
 
-        System.out.println(queryParams);
+//        System.out.println(queryParams);
         try {
             Reader reader = getScvFile(source);
             CsvParserSettings settings = new CsvParserSettings();
@@ -35,8 +44,8 @@ public class ApiController {
             CsvParser csvParser = new CsvParser(settings);
             List<String[]> allData = csvParser.parseAll(reader);
             CsvDocument csv = new CsvDocument(allData);
-            System.out.println("Parsed Content : " + csvParser.getContext().currentParsedContent());
-            System.out.println("Parsed toString : " + csvParser.toString());
+//            System.out.println("Parsed Content : " + csvParser.getContext().currentParsedContent());
+//            System.out.println("Parsed toString : " + csvParser.toString());
             String data = csv.filter(filter).sort(sort, sort_dir)
                             .get(format).orElseThrow(() -> new RuntimeException(""));
             return ResponseEntity.ok(data);
@@ -86,6 +95,10 @@ public class ApiController {
         String key = params.keySet().stream().filter(k -> k.contains("=")).findFirst().orElse(null);
 
         return null;
+    }
+
+    private String decodeValue(String value) throws UnsupportedEncodingException {
+        return URLDecoder.decode(value, StandardCharsets.UTF_8.toString());
     }
 
     private Format getFormat(String format) {
